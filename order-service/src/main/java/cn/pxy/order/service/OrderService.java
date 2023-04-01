f1a771beb7b0ce0d35f7a70e0847e7261a27dbfe
pxy <benlaixy@163.com>
2023-04-01 14:31:15 +0800

    nacos

--- order-service/src/main/java/cn/pxy/order/service/OrderService.java
+++ order-service/src/main/java/cn/pxy/order/service/OrderService.java
@@ -18,7 +18,7 @@ public class OrderService {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
         //2.利用RestTemplate发送http请求，查询用户
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        String url = "http://userservice/user/" + order.getUserId();
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
         // 4.返回
