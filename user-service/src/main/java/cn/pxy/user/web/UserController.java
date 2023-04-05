326282cc2437fd2d4e576d92a767e4e00c93c2e5
pxy <benlaixy@163.com>
2023-04-05 16:46:57 +0800

    v1

--- user-service/src/main/java/cn/pxy/user/web/UserController.java
+++ user-service/src/main/java/cn/pxy/user/web/UserController.java
@@ -5,8 +5,6 @@ import cn.pxy.user.pojo.User;
 import cn.pxy.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
